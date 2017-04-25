cb0678b17ebf9f00371e4073a19ab32313620d32
Spencer Gennari <sdgennari@gmail.com>
2017-04-24 22:23:12 -0400

    Added more sprites to level1

--- src/edu/virginia/good-dog-prototype/levels/LevelFactory.js
+++ src/edu/virginia/good-dog-prototype/levels/LevelFactory.js
@@ -37,7 +37,7 @@ class LevelFactory {
 
 			// Placeholder objects
 			// Bedroom
-			new Wall("wall11", 0, 0, 2, 1, blockSize, objColor),
+			// new Wall("wall11", 0, 0, 2, 1, blockSize, objColor),
 			new CoffeeTable("wall12", 5, 0, 1, 1, blockSize, objColor),
 			new CoffeeTable("wall13", 9, 0, 1, 1, blockSize, objColor),
 
@@ -45,13 +45,14 @@ class LevelFactory {
 			new Wall("wall14", 18, 0, 3, 1, blockSize, "#afafaf"),
 
 			// Kitchen
-			new Wall("wall11", 0, 13, 2, 1, blockSize, objColor),
-			new Wall("wall11", 12, 13, 1, 2, blockSize, objColor),
+			new Wall("wall11", 0, 13, 3, 1, blockSize, "#afafaf"),
+			new Wall("wall11", 12, 13, 1, 2, blockSize, "#afafaf"),
+			new Wall("wall11", 8, 13, 4, 1, blockSize, "#bbbbbb"),
 			new Counter("wall11", 2, 16, 1, 2, blockSize, objColor),
 			new Counter("wall11", 5, 16, 1, 2, blockSize, objColor),
 
 			// Living room
-			new Wall("wall11", 16, 12, 1, 3, blockSize, objColor),
+			// new Wall("wall11", 16, 12, 1, 3, blockSize, objColor),
 			new Table("wall11", 22, 12, 2, 3, blockSize, objColor),
 		];
 
@@ -65,7 +66,7 @@ class LevelFactory {
 
 			// Bathroom
 			LevelFactory._makePlant(16, 0, 1, 1, 2, blockSize),
-			LevelFactory._makeTub(23, 0, 2, 3, 3, blockSize),
+			LevelFactory._makeTub(23, 0.2, 2, 3, 3, blockSize),
 			LevelFactory._makeBathSink(18, 0, 1, 1, 2, blockSize),
 			LevelFactory._makeBathSink(20, 0, 1, 1, 2, blockSize),
 
@@ -73,7 +74,7 @@ class LevelFactory {
 			LevelFactory._makeStove(3, 16, 2, 2, 2, blockSize),
 			LevelFactory._makeFridge(6.5, 13, 1, 1, 2, blockSize),
 			LevelFactory._makeSink(9, 13, 3, 1, 2, blockSize),
-			LevelFactory._makeOpenableObject(12, 15, 1, 1, 2, blockSize),
+			// LevelFactory._makeOpenableObject(12, 15, 1, 1, 2, blockSize),
 
 			// Living Room
 			LevelFactory._makeCouch(21, 7, 4, 2, 2, blockSize),
@@ -277,6 +278,9 @@ class LevelFactory {
 			new Counter("counter3", 11, 3, 1, 2, blockSize, objColor),
 			new Counter("counter4", 11, 5, 1, 2, blockSize, objColor),
 
+			// Bathroom
+			new Wall("wall8", 2, 15, 3, 1, blockSize, "#afafaf"),
+
 			// Living Room
 			table,
 
@@ -300,7 +304,7 @@ class LevelFactory {
 
 			// Bathroom
 			LevelFactory._makeTub(0, 17, 2, 3, 1, blockSize),
-			LevelFactory._makeBathSink(5, 15, 1, 1, 2, blockSize),
+			LevelFactory._makeBathSink(3, 15, 1, 1, 2, blockSize),
 		];
 
 	   	// Combine the walls and interactable objects
