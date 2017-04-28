da394c98921857958118575625f08ef48b7077fc
cephalopodMD <acl3qb@virginia.edu>
2017-04-28 12:27:18 -0400

    tween transition for DollarSign

--- src/edu/virginia/good-dog-prototype/DollarSign.js
+++ src/edu/virginia/good-dog-prototype/DollarSign.js
@@ -6,12 +6,12 @@ class DollarSign extends Sprite {
     Game.getInstance().addChild(this);
     this.setPosition(x, y);
     this.setScale(.2, .2)
-    var dollarTween = new Tween(this),
+    var dollarTween = new Tween(this, TweenTransitions.quadinout),
         final = new Vec2(16, 16);
     final.transform(Game.getInstance().matrix.inverse())
-    dollarTween.animate(TweenableParams.X, x, final.x, 500);
-    dollarTween.animate(TweenableParams.Y, y, final.y, 500);
-    dollarTween.animate(TweenableParams.ALPHA, 1, 0.5, 500);
+    dollarTween.animate(TweenableParams.X, x, final.x, 800);
+    dollarTween.animate(TweenableParams.Y, y, final.y, 800);
+    dollarTween.animate(TweenableParams.ALPHA, 1, 0.2, 800);
     TweenJuggler.add(dollarTween);
     dollarTween.addEventListener(this, TweenEvent.TWEEN_COMPLETE_EVENT);
   }
